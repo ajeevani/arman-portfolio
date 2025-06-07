9da5760dfda5fc62c02ed1cf1c8447de9d5ef921
Arman Jeevani <ajeevani1@myseneca.ca>
2025-06-07 03:22:40 -0400

    update prisma.ts

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({
-  log: ['error'], // Optional: Log errors for debugging
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  log: ['error'], // Log errors for debugging
 });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
