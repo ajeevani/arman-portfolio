383279d11962d5906f17fdb20cc77ef684b7108b
Arman Jeevani <ajeevani1@myseneca.ca>
2025-06-07 03:20:28 -0400

    Added lib/prisma.ts and update route.ts

--- app/app/api/contact/route.ts
+++ app/app/api/contact/route.ts
@@ -3,8 +3,7 @@ export const dynamic = "force-dynamic";
 
 import { NextRequest, NextResponse } from 'next/server'
 import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import prisma from '@/lib/prisma';
 
 export async function POST(request: NextRequest) {
   try {
